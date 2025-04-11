a0e272739d9a675a5269838ed49ebb385764d531
appsFAO <grettel.vargasazofeifa@fao.org>
2025-04-11 16:59:42 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
+// Inicializa el mapa con vista general
 var map = L.map('map').setView([13.5, -85], 6);
 
-// Fondo satelital
+// Fondo satelital ESRI
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: '&copy; Esri, Maxar, Earthstar Geographics'
 }).addTo(map);
 
-// Capas
+// Función para popups y tooltips
+function popupGenerico(feature, layer) {
+  let props = feature.properties;
+  let contenido = '';
+  for (let key in props) {
+    contenido += `<strong>${key}:</strong> ${props[key]}<br>`;
+  }
+  layer.bindPopup(contenido);
+}
+
+// Capas inicializadas pero vacías
 var centroamerica = L.geoJSON(null, {
-  style: { color: 'blue', weight: 2 },
+  style: { color: '#0033cc', weight: 2, fillOpacity: 0.1 },
   onEachFeature: popupGenerico
 });
 
 var paisesPiloto = L.geoJSON(null, {
-  style: { color: 'orange', weight: 2, dashArray: '4' },
+  style: { color: '#ffa500', weight: 2, dashArray: '4', fillOpacity: 0.2 },
   onEachFeature: popupGenerico
 });
 
+// Cargar archivos GeoJSON
 fetch('datos/Centroamérica.geojson')
   .then(res => res.json())
   .then(data => centroamerica.addData(data));
@@ -24,9 +36,11 @@ fetch('datos/Países_piloto.geojson')
   .then(res => res.json())
   .then(data => paisesPiloto.addData(data));
 
+// Mostrar capas en el mapa al inicio
 centroamerica.addTo(map);
 paisesPiloto.addTo(map);
 
+// Control por checkbox
 document.getElementById('centroamerica').addEventListener('change', function () {
   if (this.checked) {
     centroamerica.addTo(map);
@@ -43,15 +57,7 @@ document.getElementById('paises_piloto').addEventListener('change', function () 
   }
 });
 
-function popupGenerico(feature, layer) {
-  let props = feature.properties;
-  let contenido = '';
-  for (let key in props) {
-    contenido += `<strong>${key}:</strong> ${props[key]}<br>`;
-  }
-  layer.bindPopup(contenido);
-}
-
+// Centrar por país
 function centrarEnPais(pais) {
   const coords = {
     honduras: [15.2, -86.4],
@@ -63,10 +69,12 @@ function centrarEnPais(pais) {
   }
 }
 
+// Vista general
 function vistaGeneral() {
   map.setView([13.5, -85], 6);
 }
 
+// Mostrar / ocultar panel lateral
 function togglePanel() {
   document.getElementById('panel').classList.toggle('hidden');
 }
